da343bde2bac3a6f981f8cac0e3ef0de2466e0b6
Sahil Nikshe <nikshesahil@gmail.com>
2023-12-26 02:55:30 +0530

    Changes in Router and UI

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -21,10 +21,17 @@ const Search = () => {
   // }
   return (
     <>
-      <div className="sm:align-middle flex flex-col m-5  justify-center align-middle h-50">
-        <div className="search flex  flex-col m-5 justify-center align-middle">
+      <div className="flex flex-col m-5 justify-center align-middle h-50">
+        <div className="flex flex-col  md:flex-row m-5 justify-center align-middle">
+          <img
+            className="md:w-96 sm:w-50"
+            src="https://res.cloudinary.com/dkg89jcmm/image/upload/v1703537767/loogo_fwuv3v.png"
+            alt=""
+          />
+        </div>
+        <div className="flex  flex-col md:flex-row m-5 md:m-0 md: justify-center md:gap-8 align-middle">
           <input
-            className="rounded-lg h-8"
+            className="rounded-lg h-10 md:w-96"
             type="text"
             name="query"
             id="query"
@@ -32,7 +39,7 @@ const Search = () => {
             onChange={(e) => setQuery(e.target.value)}
           />
           <Link
-            className="bg-[#a9bcd7] text-center h-10 p-2 rounded-full cursor-pointer m-6"
+            className="bg-[#a9bcd7] text-center h-10 p-2 rounded-full hover:bg-[#88a0c2] cursor-pointer m-6 md:w-60 md:m-0"
             to={"/" + query}
           >
             Search
